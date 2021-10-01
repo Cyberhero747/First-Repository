89c1c1eafb867b2203bcdaff672f1690b26208e5
Chiranjeev Vishnoi <66114276+Chiranjeev-droid@users.noreply.github.com>
2021-10-01 07:59:00 +0530

    Create palindrome_checker.py

---
+++ palindrome_checker.py
@@ -0,0 +1,15 @@
+s="saras" #enter the string here
+i=0
+j=-1
+flag=0
+
+while(i<len(s)//2):
+  if(s[i]!=s[j]):
+    flag=1
+    break
+  i+=1
+  j-=1
+if(flag==0):
+  print(s ," is palindrome..")
+else:
+  print(s, "is not palindrome")
