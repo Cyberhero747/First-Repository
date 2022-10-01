b0470fc2fb4f4db23f01b674664833d509355e09
Lakshay <113357608+lkshay21@users.noreply.github.com>
2022-10-01 06:29:23 +0530

    Update palindrome_checker.py

--- palindrome_checker.py
+++ palindrome_checker.py
@@ -1,14 +1,15 @@
 s=input("enter the string here: ")
-
-i,j,f=0,-1,0
-
-while(i<len(s)//2):
-  if(s[i]!=s[j]):
-    f=1
-    break
-  i+=1
-  j-=1
-if(f==0):
-  print(s ," is palindrome..")
+if -10<s<10:
+  print("is not palindrome")
 else:
-  print(s, "is not palindrome")
+  i,j,f=0,-1,0
+  while(i<len(s)//2):
+    if(s[i]!=s[j]):
+      f=1
+      break
+      i+=1
+      j-=1
+      if(f==0):
+        print(s ," is palindrome..")
+       else:
+         print(s, "is not palindrome")
